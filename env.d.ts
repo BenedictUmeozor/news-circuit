dd50218e24056964e0371188ba49da8e2118b29f
Benedict <benedictumeozor@gmail.com>
2024-11-03 21:36:11 +0100

    switched to using a backend

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_ACCESS_KEY: string
+  readonly VITE_API_URL: string
 }
 
 interface ImportMeta {
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,17 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: `http://api.mediastack.com/v1`,
+  baseURL: import.meta.env.VITE_API_URL,
 })
-
-api.interceptors.request.use(
-  config => {
-    const separator = config.url?.includes('?') ? '&' : '?'
-
-    config.url = `${config.url}${separator}access_key=${import.meta.env.VITE_API_ACCESS_KEY}`
-    return config
-  },
-  error => {
-    return Promise.reject(error)
-  },
-)
